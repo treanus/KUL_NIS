32bd38c45437099ef5ba6edb75e8dbb29aefae52
Stefan Sunaert <treanus@gmail.com>
2018-10-29 14:00:13 +0100

    Initial commit

---
+++ KUL_multisubjects_dcm2bids.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+# Bash shell script wrapper for calling KUL_dcm2bids for multiple subjects
+#
+# Requires KUL_dcm2bids
+#
+# @ Stefan Sunaert - UZ/KUL - stefan.sunaert@uzleuven.be
+#
+# v0.1 - dd 26/10/2018 - alpha version
+v="v0.1 - dd 26/10/2018"
+
+
+
