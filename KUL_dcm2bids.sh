8b023a5cb2b5bf9e5114903a242db1c6d2a92c0f
Stefan Sunaert <treanus@gmail.com>
2019-04-10 11:08:27 +0200

    KUL_dcm2bids: can also take a directory with dicoms as input
    This feature is undocumented

--- KUL_dcm2bids.sh
+++ KUL_dcm2bids.sh
@@ -486,26 +486,37 @@ if [ $silent -eq 0 ]; then
     echo "  The present working directory is `pwd`"
 fi
 
-# uncompress the zip file with dicoms
-kul_e2cl "  uncompressing the zip file $dcm to $tmp/$subj" $log
+# uncompress the zip file with dicoms or link the directory to tmp
+
 # clear the /tmp directory
 
 mkdir -p ${tmp}
 
-# Check the extention of the archive
-arch_ext="${dcm##*.}"
-#echo $arch_ext
+if [ -d "$dcm" ]; then
 
-if [ $arch_ext = "zip" ]; then 
-
-    unzip -q -o ${dcm} -d ${tmp}
+    # it is a directory
+    kul_e2cl "  you gave the directory $dcm as input; linking to to $tmp/$subj" $log
+    ln -s "${cwd}/${dcm}" $tmp/$subj
 
 else
 
-    tar --strip-components=5 -C ${tmp} -xzf ${dcm}
+    kul_e2cl "  uncompressing the zip file $dcm to $tmp/$subj" $log
+    # Check the extention of the archive
+    arch_ext="${dcm##*.}"
+    echo $arch_ext
+
+
+    if [ $arch_ext = "zip" ]; then 
+
+        unzip -q -o ${dcm} -d ${tmp}
+
+    elif [ $arch_ext = "tar" ]; then
+
+        tar --strip-components=5 -C ${tmp} -xzf ${dcm}
+
+    fi
 
 fi
-
 
 # dump the dicom tags of all dicoms in a file
 kul_e2cl "  brute force extraction of some relevant dicom tags of all dicom files of subject $subj into file $dump_file" $log
@@ -519,7 +530,7 @@ task(){
 
 N=4
 (
-find ${tmp} -type f | 
+find -L ${tmp} -type f | 
 while IFS= read -r dcm_file; do
     
     ((i=i%N)); ((i++==0)) && wait
@@ -531,6 +542,7 @@ done
 
 kul_e2cl "    done reading dicom tags of $dcm" $log
 wait
+
 
 # create empty bids description
 bids=""
