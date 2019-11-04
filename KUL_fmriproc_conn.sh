498a2a090bbe0dd7df37ad17985ce8c9e8d174f4
Stefan Sunaert <treanus@gmail.com>
2019-11-04 14:08:57 +0100

    KUL_frmiproc_conn: prepare files for CONN analysis

---
+++ KUL_fmriproc_conn.sh
@@ -0,0 +1,11 @@
+mkdir -p CONN/input
+cd CONN/input
+
+ln -s  ../../fmriprep/sub-*/*/func/sub-*_ses-*_task-rest_run-02_space-MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz .
+ln -s ../../fmriprep/sub-*/anat/sub-*_space-MNI152NLin6Asym_desc-preproc_T1w.nii.gz .
+ln -s ../../fmriprep/sub-*/anat/sub-*_space-MNI152NLin6Asym_label*.nii.gz .
+
+cp  fmriprep/sub-*/*/func/sub-*_ses-*_task-rest_run-02_*confounds_regressors.tsv .
+mmv "*.tsv" "#1.txt"
+
+
