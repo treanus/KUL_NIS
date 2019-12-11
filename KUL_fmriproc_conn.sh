105e3227335dffaee9ddcb4fb08301882da5918f
Stefan Sunaert <treanus@gmail.com>
2019-12-11 13:58:52 +0100

    conn update

--- KUL_fmriproc_conn.sh
+++ KUL_fmriproc_conn.sh
@@ -2,8 +2,11 @@ mkdir -p CONN/input
 cd CONN/input
 
 ln -s  ../../fmriprep/sub-*/*/func/sub-*_ses-*_task-rest_run-02_space-MNI152NLin6Asym_desc-smoothAROMAnonaggr_bold.nii.gz .
+ln -s  ../../fmriprep/sub-*/*/func/sub-*_ses-*task-rest_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz .
 ln -s ../../fmriprep/sub-*/anat/sub-*_space-MNI152NLin6Asym_desc-preproc_T1w.nii.gz .
+ln -s ../../fmriprep/sub-*/anat/sub-*_space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz .
 ln -s ../../fmriprep/sub-*/anat/sub-*_space-MNI152NLin6Asym_label*.nii.gz .
+ln -s ../../fmriprep/sub-*/anat/sub-*_space-MNI152NLin2009cAsym_label*.nii.gz .
 
 cp  fmriprep/sub-*/*/func/sub-*_ses-*_task-rest_run-02_*confounds_regressors.tsv .
 mmv "*.tsv" "#1.txt"
