9be1bd557625b4063f6ce7a0a6440ce6983c6653
Stefan Sunaert <treanus@gmail.com>
2020-12-09 16:05:47 +0100

    update

--- KUL_karawun2brainlab.sh
+++ KUL_karawun2brainlab.sh
@@ -6,51 +6,82 @@
 # @ Stefan Sunaert - UZ/KUL - stefan.sunaert@uzleuven.be
 # 07/12/2020
 
-t="_flipped"
-
-
-
-
-# Tracts should be in folder '16bit'
+#if 0; then
+# Prepare the nii files for the tracts
 mkdir -p for_elements/dti
 
-#ls 16bit/*rBO${t}.hdr
-mrconvert 16bit/*rBO${t}.img for_elements/dti/B0.nii
+# Convert the B0
+mrconvert 16bit_Elements/*rBO.img for_elements/B0.nii
 
-function kul_convert_tracts {
-    file1=16bit/*r_$1_L${t}.img
-    if [ -f $file1 ]; then
-        mrcalc $file1 1 -gt $2 -mult for_elements/dti/$1_L.nii
-    fi
-    file1=16bit/*r_$1_R${t}.img
-    if [ -f $file1 ]; then
-        mrcalc $file1 1 -gt $3 -mult for_elements/dti/$1_R.nii
-    fi
-}
+# convert each tract
+cd 16bit_Elements
+files=( $(find -name "*r_*.img" -type f -printf '%P\n') )
 
-kul_convert_tracts "Cing" 1 2
-kul_convert_tracts "CST" 3 4
-kul_convert_tracts "FA" 5 6
-kul_convert_tracts "FAT" 7 8
-kul_convert_tracts "ILFIFOF" 9 10
-kul_convert_tracts "SMACST" 11 12
-kul_convert_tracts "UNC" 13 14
 
-# fMRI Act should be in folder '16bit'
+for t in "${files[@]}"; do 
+#for t in CST_L CST_R SLF_L SLF_R IFOF_L IFOF_R ILF_L ILF_R SMAPT_L SMAPT_R FAT_L FAT_R CINGc_L CINGc_R CINGt_L CINGt_R; do
+    
+    echo $t
+    PS3="File ${t} corresponds to tract: "
+
+    select opt in Other Ignore CST_L CST_R SLF_L SLF_R IFOF_L IFOF_R \
+            ILF_L ILF_R SMAPT_L SMAPT_R FAT_L FAT_R CINGc_L \
+            CINGc_R CINGt_L CINGt_R UNC_L UNC_R; do 
+        #echo "you have selected $REPLY"
+        #echo "this is $opt"
+        color=$REPLY
+        if [ $REPLY -eq 1 ]; then
+            read -p "Describe the tract: " tract
+        elif [ $REPLY -eq 2 ]; then
+            break
+        else
+            tract=$opt
+        fi    
+        mrcalc $t 1 -gt $color -mult ../for_elements/dti/${tract}.nii
+        break
+    done
+done
+
+cd ..
+#fi
+
+# Prepare the nii for the fMRI act
 mkdir -p for_elements/fmri
-mrconvert 16bit/Anat/*anat${t}.img for_elements/fmri/T1.nii
 
-function kul_convert_fmri {
-    file1=16bit/*_act_$1${t}.img
-    echo $file1
-    ls $file1
-    if [ -f $file1 ]; then
-        mrcalc $file1 2 -gt $2 -mult for_elements/fmri/$1_L.nii
-    fi
-}
+# Convert the T1w anat
+mrconvert 16bit_Anat/*anat.img for_elements/T1.nii
 
-kul_convert_fmri "Hand" 15
-kul_convert_fmri "Lip" 16
-kul_convert_fmri "Voet" 17
-kul_convert_fmri "Taal" 18
+cd 16bit_Elements
+files=( $(find -name "*_act_*.img" -type f -printf '%P\n') )
 
+for fmri in "${files[@]}"; do 
+    echo $fmri
+    PS3="File ${fmri} corresponds to "
+
+    select opt in Other Ignore HAND LIP FOOT LANGUAGE; do
+        if [ $REPLY -eq 1 ]; then
+            read -p "Describe the task: " task
+        elif [ $REPLY -eq 2 ]; then
+            break
+        else
+            task=$opt
+        fi
+        color=$REPLY    
+        echo $task
+        echo $color
+        mrcalc $fmri 2 -gt $color -mult ../for_elements/fmri/${task}.nii
+        break
+    done
+done
+cd ..
+
+# Call Karawun
+echo "\n\n\n"
+echo "Now preparing for Karawun DICOM conversion"
+
+importTractography -d DICOM_classic/DICOM/IM_0002 -o for_elements/Upload_dti \
+    -n for_elements/B0.nii -l for_elements/dti/*.nii
+
+
+importTractography -d DICOM_classic/DICOM/IM_0002 -o for_elements/Upload_fmri \
+    -n for_elements/T1.nii -l for_elements/fmri/*.nii
