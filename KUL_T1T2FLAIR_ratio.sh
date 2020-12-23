9aca4bef43b027150351ee0eec257d9dfd327e54
Stefan Sunaert <treanus@gmail.com>
2020-12-23 21:10:19 +0100

    v0.2

--- KUL_T1T2FLAIR_ratio.sh
+++ KUL_T1T2FLAIR_ratio.sh
@@ -102,24 +102,36 @@ fi
 #	exit 2
 #fi
 
-ants_verbose=0
-# MRTRIX verbose or not?
+ants_verbose=1
+# verbose or not?
 if [ $silent -eq 1 ] ; then
-
 	export MRTRIX_QUIET=1
-    ants_verbose=1
-
+    ants_verbose=0
 fi
 
 # --- FUNCTIONS ---
+
+function KUL_antsApply_Transform {
+    antsApplyTransforms -d 3 --float 1 \
+    --verbose $ants_verbose \
+    -i $input \
+    -o $output \
+    -r $reference \
+    -t $transform \
+    -n Linear
+}
 
 # Function to do step 1
 function KUL_reorient_crop_hdbet_biascorrect_iso {
     fslreorient2std $input $outputdir/compute/${output}_std
     mrgrid $outputdir/compute/${output}_std.nii.gz crop -axis 0 $crop_x,$crop_x -axis 2 $crop_z,0 \
             $outputdir/compute/${output}_std_cropped.nii.gz
-    hd-bet -i $outputdir/compute/${output}_std_cropped.nii.gz -o $outputdir/compute/${output}_std_cropped_brain.nii.gz
+    result=$(hd-bet -i $outputdir/compute/${output}_std_cropped.nii.gz -o $outputdir/compute/${output}_std_cropped_brain.nii.gz 2>&1)
+    if [ $silent -eq 0 ]; then
+        echo $result
+    fi 
     bias_input=$outputdir/compute/${output}_std_cropped_brain.nii.gz
+    mask=$outputdir/compute/${output}_std_cropped_brain_mask.nii.gz
     bias_output=$outputdir/compute/${output}_std_cropped_brain_biascorrected.nii.gz
     N4BiasFieldCorrection --verbose $ants_verbose \
     -d 3 \
@@ -140,8 +152,37 @@ antsRegistration --verbose $ants_verbose --dimensionality 3 \
     --metric MI[$outputdir/compute/$ants_template,$outputdir/compute/$ants_source,1,32,Regular,0.25] \
     --convergence [1000x500x250x100,1e-6,10] \
     --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox
+
+    # also apply the registration to the mask
+    input=$mask
+    output=${mask##*/}
+    output=${output%%.*}
+    output=$outputdir/compute/${output}_reg2T1w.nii.gz
+    transform=$outputdir/compute/${ants_type}0GenericAffine.mat
+    reference=$outputdir/compute/$ants_template
+    #echo "input $input"
+    #echo "output $output"
+    #echo "transform $transform"
+    #echo "reference $reference"
+    KUL_antsApply_Transform
 }
 
+# Register and compute the ratio
+function KUL_register_computeratio {
+    base0=${test_T1w##*/}
+    base=${base0%_T1w*}
+    ants_type="${base}_rigid_${td}_reg2t1_"
+    ants_template="${base}_T1w_std_cropped_brain_biascorrected_iso.nii.gz"
+    ants_source="${base}_${td}_std_cropped_brain_biascorrected_iso.nii.gz"
+    newname="${base}_${td}_std_cropped_brain_biascorrected_iso_reg2T1w.nii.gz"
+    KUL_rigid_register
+    # make a better mask
+    maskfilter ${output} erode $outputdir/compute/${base}_${td}_mask_eroded.nii.gz
+    #mrcalc $outputdir/compute/$ants_template $outputdir/compute/$newname -divide \
+    #    $outputdir/${base}_T1${td}_ratio_a.nii.gz
+    mrcalc $outputdir/compute/$ants_template $outputdir/compute/$newname -divide \
+        $outputdir/compute/${base}_${td}_mask_eroded.nii.gz -multiply $outputdir/${base}_T1${td}_ratio.nii.gz
+}
 
 # --- MAIN ---
 printf "\n\n\n"
@@ -156,43 +197,40 @@ else
     T1w=($(find BIDS -type f -name "*T1w.nii.gz"))
 fi
 
-#n_T1w=${#T1w[@]}
-#echo "there are $n_T1w T1w images"
+
 d=0
 t2=0
 flair=0
 for test_T1w in ${T1w[@]}; do
 
     # Test whether T2 and/or FLAIR also exist
-    #echo "notably: $test_T1w"
     test_T2w="${test_T1w%_T1w*}_T2w.nii.gz"
-    #echo $test_T2w
     if [ -f $test_T2w ];then
         #echo "The T2 exists"
         d=$((d+1))
         t2=1
     fi
     test_FLAIR="${test_T1w%_T1w*}_FLAIR.nii.gz"
-    #echo $test_FLAIR
     if [ -f $test_FLAIR ];then
         #echo "The FLAIR exists"
         d=$((d+1))
         flair=1
     fi
 
-
     # If a T2 and/or a FLAIR exists
     if [ $d -gt 0 ]; then
-        #echo "something to do here"
+        echo "KUL_T1T2FLAIR_ratio is starting"
         mkdir -p $outputdir/compute
 
         # for the T1w
         input=$test_T1w
         output=${test_T1w##*/}
         output=${output%%.*}
-        crop_x=24
-        crop_z=48
+        echo " doing hd-bet and biascorrection on image $output"
+        crop_x=0
+        crop_z=0
         KUL_reorient_crop_hdbet_biascorrect_iso
+        mask_T1W=$mask
 
         if [ $t2 -eq 1 ];then
             input=$test_T2w
@@ -200,40 +238,28 @@ for test_T1w in ${T1w[@]}; do
             output=${output%%.*}
             crop_x=0
             crop_z=0
+            echo " doing hd-bet and biascorrection of the T2w"
             KUL_reorient_crop_hdbet_biascorrect_iso
-
-            base0=${test_T1w##*/}
-            base=${base0%_T1w*}
-            ants_type="rigid_T2w_reg2t1"
-            ants_template="${output}_T1w_std_cropped_brain_biascorrected_iso.nii.gz"
-            ants_source="${output}_T2w_std_cropped_brain_biascorrected_iso.nii.gz"
-            newname="${output}_T2w_std_cropped_brain_biascorrected_iso_reg2T1w.nii.gz"
-            KUL_rigid_register
-
-            mrcalc $outputdir/compute/$ants_template $outputdir/compute/$newname -divide  $outputdir/${base}-T1T2ratio.nii.gz
-
+            td="T2w"
+            echo " coregistering T2 to T1 and computing the ratio"
+            KUL_register_computeratio
         fi
 
-        flair=0
         if [ $flair -eq 1 ];then
             input=$test_FLAIR
             output=${test_FLAIR##*/}
+            output=${output%%.*}
             crop_x=0
             crop_z=0
-            KUL_reorient_crop_hdbet_biascorrect
-
-            base0=${test_T1w##*/}
-            base=${base0%_T1w*}
-            ants_type="rigid_FLAIR_reg2t1"
-            newname="1iso_biascorrected_brain_cropped_std_${base}_FLAIR_reg2T1w.nii.gz"
-            ants_template="iso_biascorrected_brain_cropped_std_${base}_T1w.nii.gz"
-            ants_source="iso_biascorrected_brain_cropped_std_${base}_FLAIR.nii.gz"
-            KUL_rigid_register
-
-            mrcalc $outputdir/compute/$ants_template $outputdir/compute/$newname -divide  $outputdir/${base}-T1FLAIRratio.nii.gz
-
+            echo " doing hd-bet and biascorrection of the FLAIR"
+            KUL_reorient_crop_hdbet_biascorrect_iso
+            td="FLAIR"
+            echo " coregistering FLAIR to T1 and computing the ratio"
+            KUL_register_computeratio
         fi
 
+        echo " done"
+    
     else
         echo "Nothing to do here"
     fi
